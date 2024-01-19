45884fc5579c26d40d7557575dca7cb24d8b55c1
Sam Chen <anachronicnomad@gmail.com>
2024-01-19 03:50:17 -0700

    fix Georgia

--- process_data.R
+++ process_data.R
@@ -228,7 +228,8 @@ process_data <- \(DATA_DIR) {
           gsub("(St\\. Peter's Coll)", "St. Peter's U", x = _) |>
           gsub("(MO-U of, Rolla)", "Missouri U of Sci & Tech", x = _) |>
           gsub("(Metropolitan St Coll of Denver)", "Metropolitan St U of Denver", x = _) |>
-          gsub("(Mesa State Coll)", "Colorado Mesa U", x = _)
+          gsub("(Mesa State Coll)", "Colorado Mesa U", x = _) |>
+          gsub("(Southern Polytechnic St U)", "Kennesaw State U", x = _)
     ) |>
     mutate(
       Institution = case_when(
